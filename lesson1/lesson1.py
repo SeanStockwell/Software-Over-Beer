20be9ce3b7a70475239d0fee830ae71f84e69448
SeanStockwell <seanmstockwell@gmail.com>
2019-05-24 01:03:52 -0400

    Working away at lesson 1. Simple, hardcoded neural networks. Meant to introduce people to pytorch and simple aspects of NNs

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -1,43 +1,72 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 import numpy as np
 
-################################################
-############ Lesson 1 - Universal approximation and NN basics ############
- # TODOs: 
- # 1. Decide everything this lesson should contain
- # 2. Implement things this lesson should contain
- # 3. Create a lesson1_problems.py for people to implement (like a hw assignment)
- #		- need to brainstorm what type of problems would be appropriate this early on
- # 4. Create a lesson1_soln.py to check problems for valid solutions
- # 		- may or may not release
- # 5. Have a beer and clean code up for release
- # 		- may include a lesson1_doc/pytorch_doc (for pytorch documentation)
- #
- # REMINDER: Don't reinvent the wheel. Add new + relevant info
- #
- # What this lesson contains:
- # i. An intro to pytorch
- # 		- Basics of what comprises the neural net. I will get into training and all of that later
- # ii. Demonstration of a neural network's ability to approximate
- # a few different functions
- # 		- Simple linear function(s) y = wx + b, or y = wx1 + wx2 + b
- #		- A more complicated function: y = cos(x) + x^2 or y = cos(x1) + x2^2
- #		- A function with multiple outputs... y,z = TODO
- # 
- #		- Neural network might be pretrained (by me) and included in git repo
- #		- Can plot error and all of that through matplotlib ... good chance to learn more about that
- #
- # Might be neat: Have one big, custom neural network that I build/have throughout series
- # 	- Each part of network will be color coded (line by line) that details which lesson
- #	covers that material.
- #  - Will have to decide if I want to custom build forward and backward propagation methods
- #
- ################################################ 
+
+class FeedForwardNetwork(nn.Module):
+	"""
+	Describe the network a bit here...
+	"""
+	def __init__(self, input_size=1, output_size=1):
+		super(FeedForwardNetwork, self).__init__()     # This is calling the __init__() method of the parent class (nn.module)
+		self.fc1 = nn.Linear(input_size, output_size)  # Only Fully-Connected Layer: x (input data) -> y (hidden node)
+	
+	def forward(self, x):                              # Forward pass: stacking each layer together (in this case, we just have one)
+		out = self.fc1(x)
+		return out
+
+
+def simple_linear_calculation():
+	'''
+	This is a demonstration of how you can compute a simple linear
+	equation using a linear neural network with PyTorch.
+
+	In this demo, we will be computing the equation y = 2x + 5
+	'''
+
+	# This will be our input to the FeedForwardNetwork			#
+	# Note that this is a 2d Array. The Neural Network expects 	#
+	# an array for input. 
+	x = [[2.0], [8.0], [32.0]]
+
+	x = torch.tensor(x)
+
+	# Declare the FeedForwardNetwork
+	model = FeedForwardNetwork()
+
+	# We are going to hardcode the values for the parameters of	
+	# our model. 
+
+	# Later, we will learn how these values can be 'learned'	#
+	# using data, which is what machine learning and deep		#
+	# learning is all about. For now, this is the simplest demo	#
+	# of how a neural network works.							#
+
+	# Returns list with tensors for values of W and b
+	model_params = list(model.parameters())
+
+	# Set parameter values so that linear network is equivalent
+	# to 2x + 5
+	model_params[0].data = torch.tensor([[2.0]])
+	model_params[1].data = torch.tensor([5.0])
+
+	# Now we can pass our input (x) into the model, getting our output (y)
+	output = model(x)
+	# If you're curious why we can just call model(x) and not 	#
+	# model.forward(x), check out the link below:				#
+	# https://discuss.pytorch.org/t/why-can-you-call-model-without-specifying-forward-method/24762 #
+
+	# You can verify that the y is correctly calculated for 2, 8, and 32
+	print(output.data)
 
 
 def main():
-    print("Running Lesson 1")
+	print("Running Lesson 1\n")
+	simple_linear_calculation()
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+	main()
